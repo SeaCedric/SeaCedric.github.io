12495a59079b5e14e548c8ac64c13cb0594b1da5
SeaCedric <vamos.cedric@gmail.com>
2026-02-22 20:02:52 +0100

    without modal

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    /* FILTER */
+    
     const filterButtons = document.querySelectorAll(".filter-btn");
     const projects = document.querySelectorAll(".project-card");
 
@@ -20,34 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
         });
-    });
-
-    /* MODAL */
-    const modal = document.getElementById("project-modal");
-    const modalTitle = document.getElementById("modal-title");
-    const modalDescription = document.getElementById("modal-description");
-    const modalGithub = document.getElementById("modal-github");
-    const closeModal = document.getElementById("close-modal");
-
-    projects.forEach(card => {
-        card.addEventListener("click", function () {
-
-            modalTitle.innerText = card.dataset.title;
-            modalDescription.innerText = card.dataset.description;
-            modalGithub.href = card.dataset.github;
-
-            modal.classList.add("active");
-        });
-    });
-
-    closeModal.addEventListener("click", () => {
-        modal.classList.remove("active");
-    });
-
-    modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
-            modal.classList.remove("active");
-        }
     });
 
 });
